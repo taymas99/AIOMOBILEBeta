a86f483649ead3cf34fbe1021ef8e51f3fda5efe
taymas99 <45528502+taymas99@users.noreply.github.com>
2019-01-13 21:03:34 +0600

    Add files via upload

---
+++ AIOMOBILEBeta.py
@@ -0,0 +1,46 @@
+Python 3.3.3 (v3.3.3:c3896275c0f6, Nov 18 2013, 21:19:30) [MSC v.1600 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> ================================ RESTART ================================
+>>> 
+ 
+╔═══╗╔══╗╔═══╗     ╔═╗╔═╗╔═══╗╔══╗─╔══╗╔╗───╔═══╗
+║╔═╗║╚╣─╝║╔═╗║     ║║╚╝║║║╔═╗║║╔╗║─╚╣─╝║║───║╔══╝
+║║─║║─║║─║║─║║     ║╔╗╔╗║║║─║║║╚╝╚╗─║║─║║───║╚══╗
+║╚═╝║─║║─║║─║║     ║║║║║║║║─║║║╔═╗║─║║─║║─╔╗║╔══╝
+║╔═╗║╔╣─╗║╚═╝║     ║║║║║║║╚═╝║║╚═╝║╔╣─╗║╚═╝║║╚══╗
+╚╝─╚╝╚══╝╚═══╝     ╚╝╚╝╚╝╚═══╝╚═══╝╚══╝╚═══╝╚═══╝
+========================================
+Created By Taymas and Pavvlusa
+Channel: youtube.com/UCvMv_2mSpREvi_vS1jnm4Bw
+Facebook: facebook.com/x3kakoi
+Ver: 228
+----
+DLYA USEROV KEKTEAM.NET )0))0
+----
+==========================================
+Stan mamkinum hackerom.
+------------------------------------------
+1. Install DDOS
+2. Install IPGeoLocation
+3. Install DDOS Version 2
+4. Install Metasploit
+5. Install ngrok
+6. Install Kali Nethunter
+7. Install angryFuzzer
+8. Install Red_Hawk
+9. Install Weeman
+10. Install IPGeoLocation
+11. Install Cupp
+12. Instagram Bruteforcer (instahack)
+13. Twitter Bruteforcer   (TwitterSniper)
+14. Install Ubuntu
+15. Install Fedora
+16. Install viSQL
+17. Install Hash-Buster
+18. Install D-TECT
+19. Install routersploit
+------------------------------------------
+99. Exit
+==========================================
+
+#: 
